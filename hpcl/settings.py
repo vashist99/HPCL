b9b1a75a25b44a30e3cfdc395d84be09fc05d858
vashist99 <201752040@iiitvadodara.ac.in>
2018-05-17 12:09:22 +0530

    Please enter the commit message for your changes. these are some new  commits that i missed out earlier

--- hpcl/settings.py
+++ hpcl/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'assets'),
+os.path.join(BASE_DIR,'assets '),
 
 ]
--- hpemployee/models.py
+++ hpemployee/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class hpemployee(models.Model):
     employee_name=models.CharField(max_length=100)
     location=models.TextField()
+    employee_number=models.IntegerField(default='30084769')
+    date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.employee_name
-        
